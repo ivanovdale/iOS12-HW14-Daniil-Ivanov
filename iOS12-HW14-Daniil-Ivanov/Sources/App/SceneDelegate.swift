084ed2a0eab60e4010fd9c92e1729fc7f0ece46e
ivanovdale <dania-spb@mail.ru>
2024-02-05 15:56:54 +0300

    add TabBarController

--- iOS12-HW14-Daniil-Ivanov/Sources/App/SceneDelegate.swift
+++ iOS12-HW14-Daniil-Ivanov/Sources/App/SceneDelegate.swift
@@ -12,8 +12,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
+        let tabBarController = TabBarController()
         window = UIWindow(windowScene: windowScene)
-        window?.rootViewController = ViewController()
+        window?.rootViewController = tabBarController
         window?.makeKeyAndVisible()
     }
 }
--- iOS12-HW14-Daniil-Ivanov/Sources/TabBar/EmptyViewController.swift
+++ iOS12-HW14-Daniil-Ivanov/Sources/TabBar/EmptyViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  EmptyViewController.swift
 //  iOS12-HW14-Daniil-Ivanov
 //
 //  Created by Daniil (work) on 05.02.2024.
@@ -7,9 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class EmptyViewController: UIViewController {
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .white
     }
-}
 
+}
